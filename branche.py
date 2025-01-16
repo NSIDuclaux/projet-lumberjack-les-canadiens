d68ddfb7b0eba74813a799e84a0035dac8d4aa83
MineNevs <sven.ensing@gmail.com>
2025-01-16 14:43:22 +0100

    classe branche

--- branche.py
+++ branche.py
@@ -2,10 +2,10 @@ class Branche:
 
     def __init__(self):
         self.y = 0
-        position = randint(0,1):
-            if position == 0:
-                self.x = 16
-            else :
-                self.x = 64
+        position = randint(0,1)
+        if position == 0:
+            self.x = 16
+        else :
+            self.x = 64
     
     pass
