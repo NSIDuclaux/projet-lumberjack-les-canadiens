66c6deca3700e946935f2b65e5215c89ff33c845
MineNevs <sven.ensing@gmail.com>
2025-01-16 14:41:50 +0100

    added class branche

---
+++ branche.py
@@ -0,0 +1,11 @@
+class Branche:
+
+    def __init__(self):
+        self.y = 0
+        position = randint(0,1):
+            if position == 0:
+                self.x = 16
+            else :
+                self.x = 64
+    
+    pass
